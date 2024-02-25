24a81a6fec6202f04f361bb111e4746414567aeb
Luis Silva <luis.silva@gbtembedded.com>
2024-02-25 16:03:10 +0000

    initial commit

---
+++ src/main/java/com/luisilva/transactionschedulerapp/TransactionSchedulerAppApplication.java
@@ -0,0 +1,13 @@
+package com.luisilva.transactionschedulerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSchedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSchedulerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisilva/transactionschedulerapp/TransactionSchedulerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisilva.transactionschedulerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSchedulerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
