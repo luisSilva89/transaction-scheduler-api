b2566fce26cad6eb23b6c31b86d1fc614d7ec87f
Luis Silva <luis.silva@gbtembedded.com>
2024-02-26 15:36:04 +0000

    added a README and started developing tests to cover services

---
+++ README.md
@@ -0,0 +1,29 @@
+# About The Project
+
+## Transaction Scheduler App
+
+An Api that allows to save, edit, delete and update the scheduling of bank transactions.
+To each scheduled transaction submitted, a fee will be applied based on the amount and the scheduling date.  
+
+## Built With
+
+- Technologies/Frameworks:
+    - Java 17 
+    - Spring Boot
+    - Spring Data JPA
+    - Spring Web
+    - PostgreSQL
+
+- Tools:
+    - Maven
+    - Development Tools:
+    - Lombok
+    - PgAdmin4
+
+- Testing Frameworks:
+    - JUnit
+
+- Other Libraries:
+    - ModelMapper
+
+# Getting Started
--- src/test/java/com/luisilva/transactionschedulerapp/services/TransactionServiceTest.java
+++ src/test/java/com/luisilva/transactionschedulerapp/services/TransactionServiceTest.java
@@ -1,12 +1,15 @@
 package com.luisilva.transactionschedulerapp.services;
 
 import com.luisilva.transactionschedulerapp.data.entities.ScheduledTransaction;
+import com.luisilva.transactionschedulerapp.exceptions.NoContentAtTheDatabaseException;
 import com.luisilva.transactionschedulerapp.repositories.ScheduledTransactionRepository;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -32,8 +35,13 @@ public class TransactionServiceTest {
     void shouldThrowANoContentAtTheDatabaseExceptionWhenGetAllTransactionsFromClientAccountReturnsAnEmptyList() {
 
         // Scenario
+        Long clientAccountId = 1L;
+
         List<ScheduledTransaction> clientScheduledTransactions = new ArrayList<>();
+        Mockito.when(repository.findScheduledTransactionsByClientAccountId(clientAccountId)).thenReturn(clientScheduledTransactions);
 
-
+        // Action and Asserts
+        Assert.assertThrows(NoContentAtTheDatabaseException.class, () -> service.getAllTransactionsFromClientAccount(clientAccountId));
     }
+    
 }
